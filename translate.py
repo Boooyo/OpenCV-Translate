a10eca5f1cfe868c34b57ed0252f72bf454fe941
Boo_oyo <136545068+Boooyo@users.noreply.github.com>
2024-04-09 09:31:12 +0900

    Code Edit

--- translate.py
+++ translate.py
@@ -1,28 +1,36 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('../img/fish.jpg')
-rows,cols = img.shape[0:2]  # 영상의 크기
+def translate_image(img, dx, dy, border_type=cv2.BORDER_CONSTANT, border_color=(255, 0, 0)):
+    rows, cols = img.shape[0:2]
+    
+    # 변환 행렬 생성
+    mtrx = np.float32([[1, 0, dx], [0, 1, dy]])  
 
-dx, dy = 100, 50            # 이동할 픽셀 거리
+    # 이동
+    dst = cv2.warpAffine(img, mtrx, (cols + dx, rows + dy))
 
-# ---① 변환 행렬 생성 
-mtrx = np.float32([[1, 0, dx],
-                   [0, 1, dy]])  
-# ---② 단순 이동
-dst = cv2.warpAffine(img, mtrx, (cols+dx, rows+dy))   
+    # 외곽 처리
+    if border_type == cv2.BORDER_CONSTANT:
+        dst = cv2.warpAffine(img, mtrx, (cols + dx, rows + dy), None, cv2.INTER_LINEAR, border_type, border_color)
+    elif border_type == cv2.BORDER_REFLECT:
+        dst = cv2.warpAffine(img, mtrx, (cols + dx, rows + dy), None, cv2.INTER_LINEAR, border_type)
 
-# ---③ 탈락된 외곽 픽셀을 파랑색으로 보정
-dst2 = cv2.warpAffine(img, mtrx, (cols+dx, rows+dy), None, \
-                        cv2.INTER_LINEAR, cv2.BORDER_CONSTANT, (255,0,0) )
+    return dst
 
-# ---④ 탈락된 외곽 픽셀을 원본을 반사 시켜서 보정
-dst3 = cv2.warpAffine(img, mtrx, (cols+dx, rows+dy), None, \
-                                cv2.INTER_LINEAR, cv2.BORDER_REFLECT)
+if __name__ == "__main__":
+    img = cv2.imread('../img/fish.jpg')
 
-cv2.imshow('original', img)
-cv2.imshow('trans',dst)
-cv2.imshow('BORDER_CONSTATNT', dst2)
-cv2.imshow('BORDER_FEFLECT', dst3)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    dx, dy = 100, 50  # 이동할 픽셀 거리
+
+    translated_img = translate_image(img, dx, dy)
+    border_constant_img = translate_image(img, dx, dy, cv2.BORDER_CONSTANT, (255, 0, 0))
+    border_reflect_img = translate_image(img, dx, dy, cv2.BORDER_REFLECT)
+
+    cv2.imshow('original', img)
+    cv2.imshow('translated', translated_img)
+    cv2.imshow('BORDER_CONSTANT', border_constant_img)
+    cv2.imshow('BORDER_REFLECT', border_reflect_img)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
